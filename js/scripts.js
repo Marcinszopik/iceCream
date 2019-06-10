239ec382019bc4e46aa5ada7964f71db069a3bbc
Joseph Tubridy and Marcin Szopik <marcin@>
2019-06-10 14:33:59 -0700

    add toUpperCase to grocery selector

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,24 @@ $(document).ready(function(){
     $(".iceCream").append("<li>" + iceCream + "</li>");
   });
 
+  $('.form').submit(function(event){
+
+    var groceryItems = [$("#grocery1").val(), $('#grocery2').val(), $('#grocery3').val()];
+    var groceryList = groceryItems.map(function(groceryItems){
+      return groceryItems.toUpperCase();
+    });
+    alert(groceryList)
+
+    // function myFunction() {
+    //   var text = document.getElementById("demo").innerHTML;
+    //   document.getElementById("demo").innerHTML = text.toUpperCase();
+    // }
+
+    //   alert(groceryItems);
+    //   console.log('groceryList');
+    event.preventDefault();
+    // });
 
 
-
-
-
-
+  });
 });
