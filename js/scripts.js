7f78ae8ea409afcdee3d815e19b1fe3853c854c1
Joseph Tubridy and Marcin Szopik <joseph@>
2019-06-10 10:44:19 -0700

    initial iceCream page with working java push into html

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  var iceCreams = ['mint', 'vanilla', 'chocolate'];
+  iceCreams.forEach(function(iceCream){
+    // $("." + iceCream).append("<li>" + iceCream +"</li>");
+    // console.log
+    $(".iceCream").append("<li>" + iceCream + "</li>");
+  });
+
+
+
+
+
+
+
+});
