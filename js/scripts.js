1d0cfb4ff1ba605be62e72a6dd7eaf9057098019
Joseph Tubridy and Marcin Szopik <marcin@>
2019-06-10 16:11:30 -0700

    add split sentence into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,43 @@
 $(document).ready(function(){
-  var iceCreams = ['mint', 'vanilla', 'chocolate'];
-  iceCreams.forEach(function(iceCream){
-    // $("." + iceCream).append("<li>" + iceCream +"</li>");
-    // console.log
-    $(".iceCream").append("<li>" + iceCream + "</li>");
-  });
+  // var iceCreams = ['mint', 'vanilla', 'chocolate'];
+  // iceCreams.forEach(function(iceCream){
+  //   // $("." + iceCream).append("<li>" + iceCream +"</li>");
+  //   // console.log
+  //   $(".iceCream").append("<li>" + iceCream + "</li>");
+  // });
+  //
+  // $('.form').submit(function(event){
+  //
+  //   var groceryItems = [$("#grocery1").val(), $('#grocery2').val(), $('#grocery3').val()];
+  //   var groceryList = groceryItems.map(function(groceryItems){
+  //     return groceryItems.toUpperCase();
+  //
+  //   });
+  //
+  //   groceryList.sort();
+  //   alert(groceryList);
+  //
+  //
+  //   //   alert(groceryItems);
+  //   //   console.log('groceryList');
+  //   event.preventDefault();
+  //   // });
+  //
+  //
+  // });
 
-  $('.form').submit(function(event){
+  $('.form2').submit(function(event){
 
-    var groceryItems = [$("#grocery1").val(), $('#grocery2').val(), $('#grocery3').val()];
-    var groceryList = groceryItems.map(function(groceryItems){
-      return groceryItems.toUpperCase();
-
-    });
-
-    groceryList.sort();
-    alert(groceryList);
-
-    // function myFunction() {
-    //   var text = document.getElementById("demo").innerHTML;
-    //   document.getElementById("demo").innerHTML = text.toUpperCase();
-    // }
-
-    //   alert(groceryItems);
-    //   console.log('groceryList');
+    var str = $('#wordPlay').val();
+    var splits = str.split(' ');
+    console.log(splits);
+    // var string = 'wordPlay.';
+    // var splits = string.split(' ',3);
+    // console.log(splits)
+    // var splits = string.split(' ');
+    // console.log(splits)
+    alert(splits);
     event.preventDefault();
-    // });
-
-
   });
+
 });
